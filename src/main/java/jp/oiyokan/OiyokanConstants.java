93e7c7311e998e6ddc2e8bc50a84da17e328bfe3
Toshiki Iga <igapyon@gmail.com>
2021-03-25 08:28:35 +0900

    update html. (#51)
    * update html.
    * update readme.

--- src/main/java/jp/oiyokan/OiyokanConstants.java
+++ src/main/java/jp/oiyokan/OiyokanConstants.java
@@ -27,7 +27,7 @@ public class OiyokanConstants {
     /**
      * Oiyokan のバージョン番号
      */
-    public static final String VERSION = "20210324a";
+    public static final String VERSION = "20210325a";
 
     /**
      * Oiyokan がリクエストを処理する際の 'OData v4' からはじまるトレースを出力するかどうか。
