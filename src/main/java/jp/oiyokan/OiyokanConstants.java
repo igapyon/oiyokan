b6ef21c57a4e50e8e0de3c38d60c7ff4e6c7255a
Toshiki Iga <igapyon@gmail.com>
2021-03-28 08:34:54 +0900

    save

--- src/main/java/jp/oiyokan/OiyokanConstants.java
+++ src/main/java/jp/oiyokan/OiyokanConstants.java
@@ -27,7 +27,7 @@ public class OiyokanConstants {
     /**
      * Oiyokan のバージョン番号
      */
-    public static final String VERSION = "1.0.20210328b";
+    public static final String VERSION = "1.0.20210328c";
 
     /**
      * Oiyokan がリクエストを処理する際の 'OData v4' からはじまるトレースを出力するかどうか。
