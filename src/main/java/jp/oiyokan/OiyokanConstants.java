d49d95ee980fe6befdb510cf0c1c313cf76878bf
Toshiki Iga <igapyon@gmail.com>
2021-03-28 07:06:08 +0900

    update html sample query (#66)
    * update sample query.

--- src/main/java/jp/oiyokan/OiyokanConstants.java
+++ src/main/java/jp/oiyokan/OiyokanConstants.java
@@ -27,7 +27,7 @@ public class OiyokanConstants {
     /**
      * Oiyokan のバージョン番号
      */
-    public static final String VERSION = "20210327g";
+    public static final String VERSION = "1.0.20210328a";
 
     /**
      * Oiyokan がリクエストを処理する際の 'OData v4' からはじまるトレースを出力するかどうか。
