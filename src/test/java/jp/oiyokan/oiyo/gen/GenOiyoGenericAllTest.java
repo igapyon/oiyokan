5d7f38433850548b775499cc5be94331a4d2b8fc
Toshiki Iga <igapyon@gmail.com>
2021-04-04 20:20:57 +0900

    save

--- src/test/java/jp/oiyokan/oiyo/gen/GenOiyoGenericAllTest.java
+++ src/test/java/jp/oiyokan/oiyo/gen/GenOiyoGenericAllTest.java
@@ -30,7 +30,7 @@ import jp.oiyokan.settings.OiyokanSettingsUtil;
  * 内部データベース用のCSDL用内部テーブルのDDLをコマンドライン生成.
  */
 class GenOiyoGenericAllTest {
-    private static final String TARGET_SETTINGS_DATABASE = "postgres1";
+    private static final String TARGET_SETTINGS_DATABASE = "oracle1";
 
     private static final boolean SHOW_JDBCINFO = true;
 
@@ -77,6 +77,98 @@ class GenOiyoGenericAllTest {
                     continue;
                 }
 
+                // for ORACLE XE
+                if (tableName.startsWith("ALL_") //
+                        || tableName.startsWith("ALL$") //
+                        || tableName.equals("AV_DUAL") //
+                        || tableName.equals("CATALOG") //
+                        || tableName.equals("HELP") //
+                        || tableName.equals("COL") //
+                        || tableName.equals("SYNONYMS") //
+                        || tableName.equals("SYSCATALOG") //
+                        || tableName.equals("TAB") //
+                        || tableName.equals("SYSFILES") //
+                        || tableName.equals("SYSSEGOBJ") //
+                        || tableName.equals("TABQUOTAS") //
+                        || tableName.startsWith("AW$") //
+                        || tableName.startsWith("COAD$") //
+                        || tableName.startsWith("AUDIT_") //
+                        || tableName.startsWith("DICT_") //
+                        || tableName.startsWith("DOCUMENT_") //
+                        || tableName.startsWith("REDACTION_") //
+                        || tableName.startsWith("DICTIONARY") //
+                        || tableName.startsWith("FLASHBACK_") //
+                        || tableName.startsWith("USABLE_") //
+                        || tableName.startsWith("CDB_") //
+                        || tableName.startsWith("CTX_") //
+                        || tableName.startsWith("DBA_") //
+                        || tableName.startsWith("SAM_") //
+                        || tableName.startsWith("DR$") //
+                        || tableName.startsWith("DRV$") //
+                        || tableName.startsWith("EXP") //
+                        || tableName.startsWith("JAVAS") //
+                        || tableName.startsWith("QUEUE_") //
+                        || tableName.startsWith("DBMS_") //
+                        || tableName.startsWith("AUDITABLE_") //
+                        || tableName.startsWith("SYSTEM_") //
+                        || tableName.startsWith("MY_SDO") //
+                        || tableName.startsWith("SCHEMA_") //
+                        || tableName.startsWith("COLUMN_") //
+                        || tableName.startsWith("GLOBAL_") //
+                        || tableName.startsWith("CS_") //
+                        || tableName.startsWith("INDEX_") //
+                        || tableName.startsWith("IMP") //
+                        || tableName.startsWith("EXU") //
+                        || tableName.startsWith("PUBL") //
+                        || tableName.startsWith("RDF_") //
+                        || tableName.startsWith("SI_") //
+                        || tableName.startsWith("SPD_") //
+                        || tableName.contains("$") //
+                        || tableName.startsWith("SRS") //
+                        || tableName.startsWith("GEO") //
+                        || tableName.startsWith("HS_") //
+                        || tableName.startsWith("SDO_") //
+                        || tableName.startsWith("KU_") //
+                        || tableName.startsWith("LOCAL_") //
+                        || tableName.startsWith("ORA_") //
+                        || tableName.startsWith("PRODUCT_") //
+                        || tableName.startsWith("PSTU") //
+                        || tableName.startsWith("SQT_") //
+                        || tableName.startsWith("USER_") //
+                        || tableName.startsWith("ROLE_") //
+                        || tableName.startsWith("OGIS_") //
+                        || tableName.startsWith("RULE_") //
+                        || tableName.startsWith("RESOURCE_") //
+                        || tableName.startsWith("SCHEDULER_") //
+                        || tableName.startsWith("TRANSPORTABLE_") //
+                        || tableName.startsWith("TABLE_") //
+                        || tableName.startsWith("NTV2_") //
+                        || tableName.startsWith("DM_") //
+                        || tableName.startsWith("ORDDCM_") //
+                        || tableName.startsWith("UTL_") //
+                        || tableName.startsWith("NLS_") //
+                        || tableName.startsWith("PATH_") //
+                        || tableName.startsWith("TABLESPACE_") //
+                        || tableName.startsWith("SESSION_") //
+                        || tableName.startsWith("STMT_") //
+                        || tableName.startsWith("LOADER_") //
+                        || tableName.startsWith("REPORT_") //
+                        || tableName.startsWith("DATABASE_") //
+                        || tableName.startsWith("DATAPUMP_") //
+                        || tableName.startsWith("DATA_PUMP_") //
+                        || tableName.startsWith("_") //
+                        || tableName.startsWith("WM_") //
+                        || tableName.startsWith("XDS_") //
+                        || tableName.startsWith("XDB_") //
+                        || tableName.startsWith("XDB$") //
+
+                ) {
+                    if (SHOW_JDBCINFO) {
+                        System.err.println("    skip.");
+                    }
+                    continue;
+                }
+
                 tableNameList.add(tableName);
 
                 if (SHOW_JDBCINFO) {
@@ -91,6 +183,7 @@ class GenOiyoGenericAllTest {
             System.err.println("");
 
             for (String tableName : tableNameList) {
+                // System.err.println("tabname: "+tableName);
                 System.err.println(OiyokanKanDatabase.generateCreateOiyoDdl(connTargetDb, tableName));
             }
         }
