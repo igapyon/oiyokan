0a897455340632a1c755267b384b4d1d593f30b5
Toshiki Iga <igapyon@gmail.com>
2021-04-08 20:27:43 +0900

    save3

--- src/main/java/jp/oiyokan/OiyokanConstants.java
+++ src/main/java/jp/oiyokan/OiyokanConstants.java
@@ -27,7 +27,7 @@ public class OiyokanConstants {
     /**
      * Oiyokan のバージョン番号
      */
-    public static final String VERSION = "1.1.20210408a";
+    public static final String VERSION = "1.1.20210408b";
 
     /**
      * OData のルートパス.
