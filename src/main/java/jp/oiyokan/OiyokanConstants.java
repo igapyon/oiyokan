ffd8031b3828eed70a90111419ebc0bbc44f5929
Toshiki Iga <igapyon@gmail.com>
2021-04-17 06:07:11 +0900

    Releease 1.4 リリース準備

--- src/main/java/jp/oiyokan/OiyokanConstants.java
+++ src/main/java/jp/oiyokan/OiyokanConstants.java
@@ -27,7 +27,7 @@ public class OiyokanConstants {
     /**
      * Oiyokan のバージョン番号
      */
-    public static final String VERSION = "1.3.20210416a";
+    public static final String VERSION = "1.3.20210417a";
 
     /**
      * OData のルートパス.
