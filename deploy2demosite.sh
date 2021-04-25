109872e897bb7d752ba99be4b7dd872b524b0972
Toshiki Iga <igapyon@gmail.com>
2021-04-25 09:04:25 +0900

    release works.

--- deploy2demosite.sh
+++ deploy2demosite.sh
@@ -1,5 +1,5 @@
 mvn install
 
-OIYOKAN_VERSION=1.6.20210425
+OIYOKAN_VERSION=1.7.20210425
 export OIYOKAN_VERSION
 mvn deploy:deploy-file -Durl=file:./../oiyokan-demosite/repo -Dfile=target/oiyokan-${OIYOKAN_VERSION}.jar -DgroupId=jp.oiyokan -DartifactId=oiyokan -Dpackaging=jar -Dversion=${OIYOKAN_VERSION}
