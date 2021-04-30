9fd108a5374e9b8d295a1ca02d3b4ce7bf3e4c1d
Toshiki Iga <igapyon@gmail.com>
2021-04-30 22:05:48 +0900

    version up. (#348)
    version up.

--- src/main/java/jp/oiyokan/OiyokanConstants.java
+++ src/main/java/jp/oiyokan/OiyokanConstants.java
@@ -27,7 +27,7 @@ public class OiyokanConstants {
     /**
      * Oiyokan のバージョン番号
      */
-    public static final String VERSION = "1.9.20210430e";
+    public static final String VERSION = "1.10.20210430g";
 
     /**
      * 実験的な $search 機能(全文検索)が有効化されているかどうか。
