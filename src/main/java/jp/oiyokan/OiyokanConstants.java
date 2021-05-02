0c9beb70f58eb1448cfb60ba35a8cff206a21551
Toshiki Iga <igapyon@gmail.com>
2021-05-02 18:34:31 +0900

    savaaaa passphrasee

--- src/main/java/jp/oiyokan/OiyokanConstants.java
+++ src/main/java/jp/oiyokan/OiyokanConstants.java
@@ -30,6 +30,11 @@ public class OiyokanConstants {
     public static final String VERSION = "1.10.20210502a";
 
     /**
+     * 暗号化で利用するパスフレーズ。環境変数 OIYOKAN_PASSPHRASE で上書き動作。
+     */
+    public static final String OIYOKAN_PASSPHRASE = "OIYOKAN_PASSPHRASE";
+
+    /**
      * 実験的な $search 機能(全文検索)が有効化されているかどうか。
      * 
      * v1.x (当面の間) は false. v2.x で有効化される可能性あり。
