8c1ca7e7531b3fb847a1666ff0710dfa0ac7dd37
Toshiki Iga <igapyon@gmail.com>
2021-05-09 18:33:21 +0900

    設定情報の DTO に Serializable を付与、各DBでの単体テスト実施済み。 (#423)
    * 設定情報の DTO に Serializable を付与
    * 各DBでの単体テスト実施済み

--- src/main/java/jp/oiyokan/dto/OiyoSettingsEntityType.java
+++ src/main/java/jp/oiyokan/dto/OiyoSettingsEntityType.java
@@ -15,6 +15,7 @@
  */
 package jp.oiyokan.dto;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -36,7 +37,8 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({ "name", "dbName", "keyName", "property" })
 @Generated("jsonschema2pojo")
-public class OiyoSettingsEntityType {
+public class OiyoSettingsEntityType implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @JsonProperty("name")
     private String name;
