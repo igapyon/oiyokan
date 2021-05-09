d172139bfbf9bed01522b10b1a6f8e76f9fa80d3
Toshiki Iga <igapyon@gmail.com>
2021-05-09 18:21:26 +0900

    increment version number.

--- src/main/java/jp/oiyokan/OiyokanConstants.java
+++ src/main/java/jp/oiyokan/OiyokanConstants.java
@@ -27,7 +27,7 @@ public class OiyokanConstants {
     /**
      * Oiyokan のバージョン番号
      */
-    public static final String VERSION = "1.12.20210508a";
+    public static final String VERSION = "1.13.20210509a";
 
     /**
      * 暗号化で利用するパスフレーズ。環境変数 OIYOKAN_PASSPHRASE で上書き動作。
