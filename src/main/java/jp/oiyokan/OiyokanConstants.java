9804a2faff9c8adbf182a74115177ef34bc7213e
Toshiki Iga <igapyon@gmail.com>
2021-05-10 19:51:38 +0900

    save

--- src/main/java/jp/oiyokan/OiyokanConstants.java
+++ src/main/java/jp/oiyokan/OiyokanConstants.java
@@ -27,7 +27,7 @@ public class OiyokanConstants {
     /**
      * Oiyokan のバージョン番号
      */
-    public static final String VERSION = "1.13.20210510a";
+    public static final String VERSION = "1.14.20210510a";
 
     /**
      * 暗号化で利用するパスフレーズ。環境変数 OIYOKAN_PASSPHRASE で上書き動作。
