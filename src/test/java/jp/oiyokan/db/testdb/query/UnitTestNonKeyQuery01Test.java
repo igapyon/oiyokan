6bb1642e30e8b25ae33c3976c48bf03f9fb4ed45
Toshiki Iga <igapyon@gmail.com>
2021-05-10 18:56:38 +0900

    エラーケースを追加。

---
+++ src/test/java/jp/oiyokan/db/testdb/query/UnitTestNonKeyQuery01Test.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 Toshiki Iga
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.oiyokan.db.testdb.query;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.apache.olingo.server.api.ODataResponse;
+import org.junit.jupiter.api.Test;
+
+import jp.oiyokan.OiyokanUnittestUtil;
+import jp.oiyokan.common.OiyoInfo;
+import jp.oiyokan.util.OiyokanTestUtil;
+
+/**
+ * ごく基本的な OData の挙動を確認.
+ */
+class UnitTestNonKeyQuery01Test {
+    @Test
+    void test01() throws Exception {
+        @SuppressWarnings("unused")
+        final OiyoInfo oiyoInfo = OiyokanUnittestUtil.setupUnittestDatabase();
+
+        final ODataResponse resp = OiyokanTestUtil.callGet("/ODataTest9", "$select=Name,Description");
+        final String result = OiyokanTestUtil.stream2String(resp.getContent());
+
+        assertEquals(
+                "{\"@odata.context\":\"$metadata#ODataTest1\",\"value\":[{\"ID\":1,\"Name\":\"MacBookPro16,2\",\"Description\":\"MacBook Pro (13-inch, 2020, Thunderbolt 3ポートx 4)\"}]}",
+                result);
+        assertEquals(200, resp.getStatusCode());
+    }
+}
