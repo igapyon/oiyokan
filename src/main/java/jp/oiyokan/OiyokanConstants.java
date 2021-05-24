6061c8ebac91892bc9a3aa3b2c9354bf373284a0
Toshiki Iga <igapyon@gmail.com>
2021-05-24 21:54:38 +0900

    パラメータなしクエリは java.sql.Statement を利用する  (#455)
    パラメータなしクエリは java.sql.Statement を利用する

--- src/main/java/jp/oiyokan/OiyokanConstants.java
+++ src/main/java/jp/oiyokan/OiyokanConstants.java
@@ -27,7 +27,7 @@ public class OiyokanConstants {
     /**
      * Oiyokan のバージョン番号
      */
-    public static final String VERSION = "1.16.20210522a";
+    public static final String VERSION = "1.16.20210524e";
 
     /**
      * 暗号化で利用するパスフレーズ。環境変数 OIYOKAN_PASSPHRASE で上書き動作。
